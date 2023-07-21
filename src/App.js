c4126b70f9399444db1a4985816281e16b015fc1
Kan Sophan <ytbro65@gmail.com>
2023-07-21 23:20:42 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 
 function App() {
@@ -10,27 +11,28 @@ function App() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      <table className="table-auto border-collapse border border-gray-300">
+    <div className="flex flex-col items-center justify-center bg-primary mx-20">
+    <table className="w-full table-auto border-collapse border border-blue-500 mx-auto">
         <thead>
           <tr>
-            <th className="border border-gray-300 px-4 py-2">Name</th>
-            <th className="border border-gray-300 px-4 py-2">Email</th>
-            <th className="border border-gray-300 px-4 py-2">Age</th>
+            <th className="border border-blue-500 px-4 py-2">Name</th>
+            <th className="border border-blue-500 px-4 py-2">Email</th>
+            <th className="border border-blue-500 px-4 py-2">Age</th>
           </tr>
         </thead>
         <tbody>
           {data.map(item => (
             <tr key={item.id}>
-              <td className="border border-gray-300 px-4 py-2">{item.name}</td>
-              <td className="border border-gray-300 px-4 py-2">{item.email}</td>
-              <td className="border border-gray-300 px-4 py-2">{item.age}</td>
+              <td className="border border-blue-500 px-4 py-2">{item.name}</td>
+              <td className="border border-blue-500 px-4 py-2">{item.email}</td>
+              <td className="border border-blue-500 px-4 py-2">{item.age}</td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
   );
+  
 }
 
 export default App;
